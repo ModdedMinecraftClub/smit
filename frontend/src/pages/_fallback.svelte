d90c99e6fb6c09d3bcbeb8e4cb2462d9b5b62b40
john01dav <john01dav@gmail.com>
2021-02-03 23:28:10 -0600

    Added debug and release builds to npm, and added a 404 page

---
+++ frontend/src/pages/_fallback.svelte
@@ -0,0 +1,13 @@
+<style>
+    #error-404{
+        text-align: center;
+    }
+    #elaboration{
+        font-style: italic;
+    }
+</style>
+<div id="error-404">
+    <h1>404</h1>
+    <hr>
+    <span id="elaboration">Not Found</span>
+</div>
