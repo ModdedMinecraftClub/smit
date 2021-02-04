6d3861cc4392aef3bb692eb54c92b979fd4445be
john01dav <john01dav@gmail.com>
2021-02-03 23:13:44 -0600

    Added frontend based on svelte, routify, and my custom actix-web-static-files fork

---
+++ frontend/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'source-map', //to ensure that this doesn't end up enabled in production, always disable it when you're done using it
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        // see below for an explanation
+        alias: {
+            svelte: path.resolve('node_modules', 'svelte')
+        },
+        extensions: ['.mjs', '.js', '.svelte'],
+        mainFields: ['svelte', 'browser', 'module', 'main']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(html|svelte)$/,
+                use: 'svelte-loader'
+            },
+            {
+                // required to prevent errors from Svelte on Webpack 5+, omit on Webpack 4
+                test: /node_modules\/svelte\/.*\.mjs$/,
+                resolve: {
+                    fullySpecified: false
+                }
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin()]
+};
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod config;
 
 use crate::config::{yaml_from_file_or_create, Config};
-use actix_files::Files;
 use actix_web::{App, HttpServer};
+use actix_web_static_files::{build_hashmap_from_included_dir, include_dir, Dir, ResourceFiles};
+
+const FRONTEND_DIR: Dir = include_dir!("frontend/dist");
 
 #[actix_web::main]
 async fn main() {
@@ -10,10 +12,13 @@ async fn main() {
         .expect("Failed to load or create config.yml.");
     println!("Bind address is: {}", config.bind_address);
 
-    HttpServer::new(|| App::new().service(Files::new("/", "frontend/dist")))
-        .bind(&config.bind_address)
-        .expect("Failed to bind to address")
-        .run()
-        .await
-        .expect("Failed to run Actix.");
+    HttpServer::new(|| {
+        let frontend_hash_map = build_hashmap_from_included_dir(&FRONTEND_DIR);
+        App::new().service(ResourceFiles::new("/", frontend_hash_map).resolve_not_found_to_root())
+    })
+    .bind(&config.bind_address)
+    .expect("Failed to bind to address")
+    .run()
+    .await
+    .expect("Failed to run Actix.");
 }
