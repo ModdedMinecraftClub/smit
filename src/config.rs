53ca577aa1362a3de9bd3f353c034b7613b450c3
john01dav <john01dav@gmail.com>
2021-02-13 21:47:51 -0600

    Added database connectivity

--- src/config.rs
+++ src/config.rs
@@ -5,15 +5,36 @@ use std::fs::{read_to_string, File};
 use std::io::Write;
 use std::path::Path;
 
+#[derive(Serialize, Deserialize)]
+pub struct MySqlCredentials {
+    pub database: String,
+    pub server: String,
+    pub username: String,
+    pub password: String,
+}
+
 #[derive(Deserialize, Serialize)]
 pub struct Config {
     pub bind_address: String,
+    pub mysql: MySqlCredentials,
+}
+
+impl Default for MySqlCredentials {
+    fn default() -> Self {
+        Self {
+            database: "smit".into(),
+            server: "localhost".into(),
+            username: "smit".into(),
+            password: "".into(),
+        }
+    }
 }
 
 impl Default for Config {
     fn default() -> Self {
         Config {
             bind_address: "127.0.0.1:8192".into(),
+            mysql: MySqlCredentials::default(),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod config;
 use crate::config::{yaml_from_file_or_create, Config};
 use actix_web::{App, HttpServer};
 use actix_web_static_files::{build_hashmap_from_included_dir, include_dir, Dir, ResourceFiles};
+use sqlx::MySqlPool;
 
 const FRONTEND_DIR: Dir = include_dir!("frontend/dist");
 
@@ -12,9 +13,18 @@ async fn main() {
         .expect("Failed to load or create config.yml.");
     println!("Bind address is: {}", config.bind_address);
 
-    HttpServer::new(|| {
+    let pool = MySqlPool::connect(&format!(
+        r#"mysql://{}:{}@{}/{}"#,
+        config.mysql.username, config.mysql.password, config.mysql.server, config.mysql.database
+    ))
+    .await
+    .expect("Failed to connect to MySql database.");
+
+    HttpServer::new(move || {
         let frontend_hash_map = build_hashmap_from_included_dir(&FRONTEND_DIR);
-        App::new().service(ResourceFiles::new("/", frontend_hash_map).resolve_not_found_to_root())
+        App::new()
+            .data(pool.clone())
+            .service(ResourceFiles::new("/", frontend_hash_map).resolve_not_found_to_root())
     })
     .bind(&config.bind_address)
     .expect("Failed to bind to address")
